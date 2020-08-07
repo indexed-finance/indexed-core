644f52200998e32b7a2c119ed7a78500fef9164b
dillon <dillonkellar@protonmail.com>
2020-08-07 17:24:27 -0500

    Added mock ERC20 contract for tests

---
+++ contracts/mocks/MockERC20.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.6.0;
+
+import "../openzeppelin/BaseERC20.sol";
+
+contract MockERC20 is BaseERC20 {
+  constructor(
+    string memory name,
+    string memory symbol
+  ) public BaseERC20(name, symbol) {}
+
+  function getFreeTokens(address to, uint256 amount) public {
+    _mint(to, amount);
+  }
+
+  /**
+   * @dev Creates `amount` tokens and assigns them to `account`, increasing
+   * the total supply.
+   * Emits a {Transfer} event with `from` set to the zero address.
+   *
+   * Requirements:
+   * - `to` cannot be the zero address.
+   */
+  function _mint(address account, uint256 amount) internal virtual {
+    require(account != address(0), "ERC20: mint to the zero address");
+    _totalSupply = _totalSupply.add(amount);
+    _balances[account] = _balances[account].add(amount);
+    emit Transfer(address(0), account, amount);
+  }
+}
