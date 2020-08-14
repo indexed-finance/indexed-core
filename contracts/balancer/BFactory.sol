800af926e12e45693869904b024a634b081a05a2
dillon <dillonkellar@protonmail.com>
2020-08-13 21:35:57 -0500

    updated BFactory to act as the base contract for the pool controller

--- contracts/balancer/BFactory.sol
+++ contracts/balancer/BFactory.sol
@@ -3,46 +3,40 @@ pragma solidity ^0.6.0;
 
 // Builds new BPools, logging their addresses and providing `isBPool(address) -> (bool)`
 
-import "./BPool.sol";
+import { BBronze, BPool, IERC20 } from "./BPool.sol";
 import "../lib/ProxyLib.sol";
 
 
 contract BFactory is BBronze {
-  event LOG_NEW_POOL(address indexed caller, address indexed pool);
+  event LOG_NEW_POOL(address indexed caller, address indexed pool, uint256 categoryID, uint256 indexSize);
 
   event LOG_BLABS(address indexed caller, address indexed blabs);
 
-  mapping(address => bool) private _isBPool;
+  mapping(address => bool) internal _isBPool;
+
+  address internal _blabs;
+  address internal _poolContract;
 
   function isBPool(address b) external view returns (bool) {
     return _isBPool[b];
   }
 
-  function newBPool(
-    uint256 categoryID,
-    uint256 indexSize,
-    string calldata name,
-    string calldata symbol
-  ) external returns (BPool) {
-    bytes32 salt = keccak256(abi.encodePacked(categoryID, indexSize));
-    address bpoolAddress = ProxyLib.deployProxy(_poolContract, salt);
-    BPool bpool = BPool(bpoolAddress);
-    bpool.initialize(
-      msg.sender,
-      name,
-      symbol
-    );
-    _isBPool[bpoolAddress] = true;
-    emit LOG_NEW_POOL(msg.sender, bpoolAddress);
-    return bpool;
-  }
-
-  address private _blabs;
-  address private _poolContract;
-
   constructor() public {
     _blabs = msg.sender;
     _poolContract = address(new BPool());
+  }
+
+  function _newBPool(
+    uint256 categoryID,
+    uint256 indexSize
+  ) internal returns (BPool) {
+    bytes32 salt = keccak256(abi.encodePacked(categoryID, indexSize));
+    address bpoolAddress = ProxyLib.deployProxy(_poolContract, salt);
+    require(!_isBPool[bpoolAddress], "Pool already deployed.");
+    BPool bpool = BPool(bpoolAddress);
+    _isBPool[bpoolAddress] = true;
+    emit LOG_NEW_POOL(msg.sender, bpoolAddress, categoryID, indexSize);
+    return bpool;
   }
 
   function getBLabs() external view returns (address) {
