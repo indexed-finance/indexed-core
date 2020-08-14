c89b068b83cfc6d345e811b5f98498b1d2cc2344
dillon <dillonkellar@protonmail.com>
2020-08-13 21:39:19 -0500

    added optimizer to buidler solc options

--- buidler.config.js
+++ buidler.config.js
@@ -19,5 +19,9 @@ module.exports = {
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
     version: "0.6.8",
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
   },
 };
