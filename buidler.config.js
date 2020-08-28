bfe8286756cb14ac543ae7214d51c5668aa089ef
dillon <dillonkellar@protonmail.com>
2020-08-28 00:04:44 -0500

    Added js helper libraries

--- buidler.config.js
+++ buidler.config.js
@@ -1,16 +1,6 @@
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nomiclabs/buidler-web3");
 
-// This is a sample Buidler task. To learn how to create your own go to
-// https://buidler.dev/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.getAddress());
-  }
-});
-
 // You have to export an object to set up your config
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
