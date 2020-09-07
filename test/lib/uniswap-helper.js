c29fda1c256c0e12fd1b4ff619e39195d1cd8a8f
dillon <dillonkellar@protonmail.com>
2020-09-06 23:02:42 -0500

    Replaced stablecoin with weth

--- test/lib/uniswap-helper.js
+++ test/lib/uniswap-helper.js
@@ -21,7 +21,7 @@ class UniswapHelper {
     } */
     const contracts = await setupUniSwapV2(this.web3, this.from);
     Object.assign(this, contracts);
-    this.stablecoin = await this.erc20Factory.deploy("DAI Stablecoin", "DAI");
+    // this.stablecoin = await this.erc20Factory.deploy("DAI Stablecoin", "DAI");
   }
 
   getToken(symbol) {
@@ -35,40 +35,51 @@ class UniswapHelper {
     }
   }
 
+  async getFreeWeth(to, amount) {
+    await this.weth.methods.deposit().send({
+      from: this.from,
+      value: amount
+    });
+    if (to && to != this.from) {
+      await this.weth.methods.transfer(to, amount).send({ from: this.from });
+    }
+  }
+
   /**
-   * Add liquidity to uniswap market pair for a token and stablecoin
-   * @param price Amount of stablecoin per token
+   * Add liquidity to uniswap market pair for a token and weth
+   * @param price Amount of weth per token
    * @param liquidity Amount of tokens to add
    */
   async addTokenLiquidity(symbol, price, liquidity) {
     const token = this.getToken(symbol).token;
     const amountToken = nTokensHex(liquidity);
-    const amountStablecoin = nTokensHex(liquidity * price);
+    const amountWeth = nTokensHex(liquidity * price);
     await token.getFreeTokens(this.from, amountToken).then(r => r.wait && r.wait());
-    await this.stablecoin.getFreeTokens(this.from, amountStablecoin).then(r => r.wait && r.wait());
+    await this.getFreeWeth(this.from, amountWeth);
+    // await this.weth.getFreeTokens(this.from, amountWeth).then(r => r.wait && r.wait());
     await token.approve(this.uniswapRouter.options.address, amountToken).then(r => r.wait && r.wait());
-    await this.stablecoin.approve(this.uniswapRouter.options.address, amountStablecoin).then(r => r.wait && r.wait());
+    await this.weth.methods.approve(this.uniswapRouter.options.address, amountWeth).send({ from: this.from });
     const timestamp = this.getTimestamp() + 1000;
     await this.uniswapRouter.methods.addLiquidity(
       token.address,
-      this.stablecoin.address,
+      this.weth.options.address,
       amountToken,
-      amountStablecoin,
+      amountWeth,
       amountToken,
-      amountStablecoin,
+      amountWeth,
       this.from,
       timestamp
     ).send({ from: this.from });
   }
 
   // Deploys an ERC20 and creates a uniswap market between it
-  // and the selected stablecoin
+  // and weth
   async deployTokenAndMarket(name, symbol, initialPrice, liquidity) {
     const token = await this.erc20Factory.deploy(name, symbol);
     // const { address, initialPrice, token } = tokenObj;
     const result = await this.uniswapFactory.methods.createPair(
       token.address,
-      this.stablecoin.address
+      this.weth.options.address
     ).send({ from: this.from });
     const { pair } = result.events.PairCreated.returnValues;
     const tokenObj = {
