0d18db010174018805a33e07e12090fdfcc2dfdf
dillon <dillonkellar@protonmail.com>
2020-09-13 02:13:10 -0500

    refactored for better organization

--- contracts/MarketOracle.sol
+++ contracts/MarketOracle.sol
@@ -5,15 +5,20 @@ import "./UniSwapV2PriceOracle.sol";
 
 
 contract MarketOracle is UniSwapV2PriceOracle {
-  // Address that can update the categories.
-  address public manager;
-  // Number of categories in the oracle.
-  uint256 public categoryIndex = 1;
+/* ---  Constants  --- */
 
   // Max time between a category being sorted and a query for the top n tokens.
   uint256 internal constant MAX_SORT_DELAY = 1 days;
   // Maximum number of tokens in a category.
   uint256 internal constant MAX_CATEGORY_TOKENS = 15;
+
+/* ---  Events  --- */
+
+  event CategoryAdded(uint256 categoryID, bytes32 metadataHash);
+  event TokenAdded(address token, uint256 categoryID);
+  event CategorySorted(uint256 categoryID);
+
+/* ---  Storage  --- */
 
   // Array of tokens for each category.
   mapping(uint256 => address[]) internal _categoryTokens;
@@ -23,10 +28,10 @@ contract MarketOracle is UniSwapV2PriceOracle {
   mapping(uint256 => bytes32) public categoryMetadata;
   // Last time a category was sorted
   mapping(uint256 => uint256) public lastCategoryUpdate;
-
-  event CategoryAdded(uint256 categoryID, bytes32 metadataHash);
-  event TokenAdded(address token, uint256 categoryID);
-  event CategorySorted(uint256 categoryID);
+  // Address that can update the categories.
+  address public manager;
+  // Number of categories in the oracle.
+  uint256 public categoryIndex = 1;
 
   constructor(
     address _uniswapFactory,
@@ -36,22 +41,34 @@ contract MarketOracle is UniSwapV2PriceOracle {
     manager = _manager;
   }
 
+/* ---  Modifiers  --- */
+
   modifier onlyManager {
     require(msg.sender == manager, "Only the manager can call this.");
     _;
   }
 
-  /* <-- CATEGORY QUERIES --> */
+/* ---  Category Queries  --- */
 
+  /**
+   * @dev Returns a boolean stating whether a category exists.
+   */
   function hasCategory(uint256 categoryID) external view returns (bool) {
     return categoryID < categoryIndex && categoryID > 0;
   }
 
   /**
-   * @dev Return the array of tokens for a category.
+   * @dev Returns the array of tokens in a category.
    */
   function getCategoryTokens(uint256 categoryID)
-  external view returns (address[] memory tokens) {
+    external
+    view
+    returns (address[] memory tokens)
+  {
+    require(
+      categoryID < categoryIndex && categoryID > 0,
+      "ERR_CATEGORY_ID"
+    );
     address[] storage _tokens = _categoryTokens[categoryID];
     tokens = new address[](_tokens.length);
     for (uint256 i = 0; i < tokens.length; i++) {
@@ -68,7 +85,36 @@ contract MarketOracle is UniSwapV2PriceOracle {
     return computeAverageMarketCaps(_categoryTokens[categoryID]);
   }
 
-  /* <-- ORACLE MANAGEMENT ACTIONS --> */
+  /**
+   * @dev Get the top `num` tokens in a category.
+   *
+   * Note: The category must have been sorted by market cap
+   * in the last `MAX_SORT_DELAY` seconds.
+   */
+  function getTopCategoryTokens(uint256 categoryID, uint256 num)
+    external
+    view
+    returns (address[] memory tokens)
+  {
+    require(
+      categoryID < categoryIndex && categoryID > 0,
+      "ERR_CATEGORY_ID"
+    );
+    address[] storage categoryTokens = _categoryTokens[categoryID];
+    require(
+      num <= categoryTokens.length,
+      "ERR_CATEGORY_SIZE"
+    );
+    require(
+      now - lastCategoryUpdate[categoryID] <= MAX_SORT_DELAY,
+      "ERR_CATEGORY_NOT_READY"
+    );
+    tokens = new address[](num);
+    for (uint256 i = 0; i < num; i++) tokens[i] = categoryTokens[i];
+  }
+
+/* ---  Category Management Actions  --- */
+
   /**
    * @dev Create a new token category.
    * @param metadataHash Hash of metadata about the token category,
@@ -82,26 +128,15 @@ contract MarketOracle is UniSwapV2PriceOracle {
 
   /**
    * @dev Adds a new token to a category.
-   */
-  function _addToken(address token, uint256 categoryID) internal {
-    require(_tokenCategories[token] == 0, "ERR_TOKEN_EXISTS");
-    _tokenCategories[token] = categoryID;
-    _categoryTokens[categoryID].push(token);
-    updatePrice(token);
-    emit TokenAdded(token, categoryID);
-  }
-
-  /**
-   * @dev Adds a new token to a category.
    * Note: A token can only be assigned to one category at a time.
    */
   function addToken(address token, uint256 categoryID) public onlyManager {
     require(categoryID < categoryIndex, "ERR_CATEGORY_ID");
-    _addToken(token, categoryID);
-    require
-      (_categoryTokens[categoryID].length <= MAX_CATEGORY_TOKENS,
+    require(
+      _categoryTokens[categoryID].length < MAX_CATEGORY_TOKENS,
       "ERR_MAX_CATEGORY_TOKENS"
     );
+    _addToken(token, categoryID);
     // Decrement the timestamp for the last category sort to ensure
     // the new token is sorted before the top n tokens can be queried.
     lastCategoryUpdate[categoryID] -= MAX_SORT_DELAY;
@@ -116,20 +151,22 @@ contract MarketOracle is UniSwapV2PriceOracle {
     uint256 categoryID,
     address[] calldata tokens
   ) external onlyManager {
-    require(categoryID < categoryIndex, "ERR_CATEGORY_ID");
+    require(
+      categoryID < categoryIndex && categoryID > 0,
+      "ERR_CATEGORY_ID"
+    );
+    require(
+      _categoryTokens[categoryID].length + tokens.length <= MAX_CATEGORY_TOKENS,
+      "ERR_MAX_CATEGORY_TOKENS"
+    );
     for (uint256 i = 0; i < tokens.length; i++) {
       _addToken(tokens[i], categoryID);
     }
-    require(
-      _categoryTokens[categoryID].length <= MAX_CATEGORY_TOKENS,
-      "ERR_MAX_CATEGORY_TOKENS"
-    );
     // Decrement the timestamp for the last category sort to ensure
     // the new token is sorted before the top n tokens can be queried.
     lastCategoryUpdate[categoryID] -= MAX_SORT_DELAY;
   }
 
-  /* <-- TOKEN SORTING --> */
   /**
    * @dev Update the order of tokens in a category by descending market cap.
    * @param categoryID Category to sort
@@ -173,23 +210,16 @@ contract MarketOracle is UniSwapV2PriceOracle {
     emit CategorySorted(categoryID);
   }
 
+/* ---  Internal Category Management Functions  --- */
+
   /**
-   * @dev Get the top tokens in a category.
-   * Note: The category must have been sorted by market cap
-   * in the last `MAX_SORT_DELAY` seconds.
+   * @dev Adds a new token to a category.
    */
-  function getTopCategoryTokens(uint256 categoryID, uint256 num)
-  external view returns (address[] memory tokens) {
-    address[] storage categoryTokens = _categoryTokens[categoryID];
-    require(
-      num <= categoryTokens.length,
-      "Category does not have sufficient tokens."
-    );
-    require(
-      now - lastCategoryUpdate[categoryID] <= MAX_SORT_DELAY,
-      "Category not sorted recently."
-    );
-    tokens = new address[](num);
-    for (uint256 i = 0; i < num; i++) tokens[i] = categoryTokens[i];
+  function _addToken(address token, uint256 categoryID) internal {
+    require(_tokenCategories[token] == 0, "ERR_TOKEN_EXISTS");
+    _tokenCategories[token] = categoryID;
+    _categoryTokens[categoryID].push(token);
+    updatePrice(token);
+    emit TokenAdded(token, categoryID);
   }
 }
