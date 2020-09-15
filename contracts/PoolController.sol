88a4ecc42c37a4d9b265183239e96ddb3d824c8c
dillon <dillonkellar@protonmail.com>
2020-09-15 12:55:55 -0500

    organization, proxy manager, token purchaser
    Organized the code structure. Made all address variables immutable, as the contracts should all be static or upgradeable delegatecall proxies. Replaced ProxyLib usage with DelegateCallProxyManager. Implemented prepared index pool deployment that buys tokens from UniSwap.

--- contracts/PoolController.sol
+++ contracts/PoolController.sol
@@ -5,28 +5,69 @@ import { BPool } from "./balancer/BPool.sol";
 import "./balancer/BNum.sol";
 import "./interfaces/IERC20.sol";
 import "./lib/FixedPoint.sol";
-import "./lib/ProxyLib.sol";
+import "./lib/Create2.sol";
 import { IndexLibrary as Index } from "./lib/IndexLibrary.sol";
 import { MarketOracle } from "./MarketOracle.sol";
-
+import { RestrictedTokenBuyer } from "./RestrictedTokenBuyer.sol";
+import "./openzeppelin/SafeERC20.sol";
+import {
+  DelegateCallProxyManager
+} from "./proxies/DelegateCallProxyManager.sol";
+import {
+  DelegateCallProxyManyToOne
+} from "./proxies/DelegateCallProxyManyToOne.sol";
 
 contract PoolController is BNum {
   using FixedPoint for FixedPoint.uq112x112;
   using FixedPoint for FixedPoint.uq144x112;
+  using SafeERC20 for IERC20;
 
 /* ---  Constants  --- */
-
+  bytes32 internal constant PROXY_CODEHASH = keccak256(
+    type(DelegateCallProxyManyToOne).creationCode
+  );
+  bytes32 internal constant POOL_IMPLEMENTATION_ID = keccak256(
+    "INDEX_POOL"
+  );
   uint256 internal constant WEIGHT_MULTIPLIER = BONE * 25;
   // Seconds between reweigh/reindex calls.
   uint256 internal constant POOL_REWEIGH_DELAY = 14 days;
   // The number of reweighs which occur before a pool is re-indexed.
   uint256 internal constant REWEIGHS_BEFORE_REINDEX = 3;
+  uint128 internal constant MAX_UINT_128 = 2**128 - 1;
+
+  address internal immutable _owner;
+  address internal immutable _weth;
+  MarketOracle public immutable _oracle;
+  RestrictedTokenBuyer public immutable _tokenBuyer;
+  DelegateCallProxyManager internal immutable _proxyManager;
 
 /* ---  Structs  --- */
 
+  /**
+   * @dev Data structure with the number of times a pool has been
+   * either reweighed or re-indexed, as well as the timestamp of
+   * the last such action.
+   *
+   * If `++index % REWEIGHS_BEFORE_REINDEX + 1` is 0, the pool will
+   * re-index, otherwise it will reweigh.
+   *
+   * @param index Number of times the pool has either re-weighed or
+   * re-indexed.
+   * @param timestamp Timestamp of last pool re-weigh or re-index.
+   */
   struct PoolUpdateRecord {
     uint128 index;
     uint128 timestamp;
+  }
+
+  /**
+   * @dev Data structure with the tokens & balances for a pool
+   * which has not yet been deployed.
+   */
+  struct PendingPool {
+    address[] tokens;
+    uint128[] balances;
   }
 
 /* ---  Events  --- */
@@ -38,43 +79,140 @@ contract PoolController is BNum {
     uint256 indexSize
   );
 
-  event LOG_MANAGER(address manager);
-
 /* ---  Storage  --- */
 
-  address internal _manager;
   address internal _poolContract;
   mapping(address => bool) internal _isBPool;
   mapping(address => PoolUpdateRecord) internal _poolUpdateRecords;
-  MarketOracle public oracle;
+  mapping(address => PendingPool) internal _pendingPools;
 
 /* ---  Modifiers  --- */
 
-  modifier onlyManager {
-    require(msg.sender == _manager, "ERR_ONLY_MANAGER");
+  modifier _owner_ {
+    require(msg.sender == _owner, "ERR_ONLY_OWNER");
     _;
   }
 
-  constructor(address _oracle, address poolContract) public {
-    _manager = msg.sender;
-    oracle = MarketOracle(_oracle);
-    _poolContract = poolContract;
-    emit LOG_MANAGER(msg.sender);
-  }
-
-  function setManager(address newManager) external onlyManager {
-    require(newManager != address(0), "ERR_NULL_ADDRESS");
-    _manager = newManager;
-    emit LOG_MANAGER(newManager);
+  constructor(
+    address owner,
+    MarketOracle oracle,
+    DelegateCallProxyManager proxyManager,
+    address weth,
+    RestrictedTokenBuyer tokenBuyer
+  ) public {
+    _owner = owner;
+    _oracle = oracle;
+    _proxyManager = proxyManager;
+    _weth = weth;
+    _tokenBuyer = tokenBuyer;
   }
 
 /* ---  Pool Actions  --- */
 
   /**
-   * @dev Deploy a new indexed pool with the category ID and index size.
+   * @dev Prepare an index pool for deployment.
+   * Calculates the initial desired weights and balances for the pool,
+   * then sends `initialWethValue` to the token buyer contract to purchase
+   * the necessary tokens over time from UniSwap.
+   */
+  function prepareIndexPool(
+    uint256 categoryID,
+    uint256 indexSize,
+    uint256 initialWethValue
+  ) external _owner_ {
+    require(indexSize >= MIN_BOUND_TOKENS, "Less than minimum index size.");
+    require(indexSize <= MAX_BOUND_TOKENS, "Exceeds maximum index size");
+    require(_oracle.hasCategory(categoryID), "ERR_CATEGORY_ID");
+    address poolAddress = computePoolAddress(categoryID, indexSize);
+    require(!_isBPool[poolAddress], "ERR_POOL_EXISTS");
+    (
+      address[] memory tokens,
+      uint256[] memory balances
+    ) = getInitialTokensAndBalances(
+      categoryID,
+      indexSize,
+      initialWethValue
+    );
+    IERC20(_weth).transfer(
+      address(_tokenBuyer),
+      initialWethValue
+    );
+    _tokenBuyer.addDesiredTokens(tokens, balances);
+    uint128[] memory smolBalances = new uint128[](indexSize);
+    for (uint256 i = 0; i < indexSize; i++) {
+      uint256 bal = balances[i];
+      require(bal <= MAX_UINT_128, "ERR_MAX_UINT_128");
+      smolBalances[i] = uint128(bal);
+    }
+    _pendingPools[poolAddress] = PendingPool(tokens, smolBalances);
+  }
+
+  /**
+   * @dev Deploy an index pool which has already been prepared with `prepareIndexPool`.
+   * Withdraws the requisite balances from the token buyer contract and calculates
+   * the weights based on the actual values of each token balance.
    *
    * TODO: Currently this just assumes that the pool controller already owns the tokens.
    * This should probably be updated to gradually purchase tokens from UniSwap.
+   *
+   * @param categoryID Identifier for the indexed category
+   * @param indexSize Number of tokens to hold in the index fund
+   * @param name Name of the index token - should indicate the category and size
+   * @param symbol Symbol for the index token
+   */
+  function deployPreparedIndexPool(
+    uint256 categoryID,
+    uint256 indexSize,
+    string calldata name,
+    string calldata symbol
+  ) external {
+    address poolAddress = computePoolAddress(categoryID, indexSize);
+    PendingPool memory poolData = _pendingPools[poolAddress];
+    require(poolData.tokens.length == indexSize, "ERR_POOL_DATA");
+    uint256[] memory balances = _to256Array(poolData.balances);
+    _tokenBuyer.withdraw(poolData.tokens, balances);
+    uint96[] memory denormalizedWeights = new uint96[](indexSize);
+    uint256[] memory ethValues = new uint256[](indexSize);
+    uint256 valueSum;
+    for (uint256 i = 0; i < indexSize; i++) {
+      address token = poolData.tokens[i];
+      uint256 balance = poolData.balances[i];
+      uint256 value = _oracle.computeAverageAmountOut(
+        token,
+        balance
+      );
+      valueSum += value;
+      ethValues[i] = value;
+      IERC20(token).safeTransferFrom(msg.sender, address(this), balance);
+      IERC20(token).safeApprove(poolAddress, balance);
+    }
+    for (uint256 i = 0; i < indexSize; i++) {
+      denormalizedWeights[i] = _denormalizeFractionalWeight(
+        FixedPoint.fraction(uint112(ethValues[i]), uint112(valueSum))
+      );
+    }
+    _deployPool(categoryID, indexSize);
+    _isBPool[poolAddress] = true;
+    emit LOG_NEW_POOL(msg.sender, poolAddress, categoryID, indexSize);
+    BPool(poolAddress).initialize(
+      address(this),
+      name,
+      symbol,
+      poolData.tokens,
+      balances,
+      denormalizedWeights
+    );
+  }
+
+    /**
+   * @dev Deploy a new index pool using tokens transferred from the caller.
+   *
+   * Note: The caller must have approved the pool controller to transfer
+   * the underlying tokens. The tokens and balances can be queried with
+   * `getInitialTokenWeightsAndBalances`, but the exact token amounts
+   * may change slightly between the query and the call to this function.
+   * The caller should either query those amounts and call this function
+   * in the same transaction or increase the queried values by a few percent.
    *
    * @param categoryID Identifier for the indexed category
    * @param indexSize Number of tokens to hold in the index fund
@@ -88,12 +226,11 @@ contract PoolController is BNum {
     string calldata name,
     string calldata symbol,
     uint256 initialWethValue
-  ) external onlyManager {
+  ) external _owner_ {
     require(indexSize >= MIN_BOUND_TOKENS, "Less than minimum index size.");
     require(indexSize <= MAX_BOUND_TOKENS, "Exceeds maximum index size");
-    require(oracle.hasCategory(categoryID), "Category does not exist");
-    bytes32 salt = keccak256(abi.encodePacked(categoryID, indexSize));
-    address bpoolAddress = ProxyLib.deployProxy(_poolContract, salt);
+    require(_oracle.hasCategory(categoryID), "Category does not exist");
+    address bpoolAddress = _deployPool(categoryID, indexSize);
     _isBPool[bpoolAddress] = true;
     emit LOG_NEW_POOL(msg.sender, bpoolAddress, categoryID, indexSize);
     BPool bpool = BPool(bpoolAddress);
@@ -120,7 +257,7 @@ contract PoolController is BNum {
   }
 
   /**
-   * @dev Re-index a pool by setting the underlying assets to the top
+   * @dev Re-indexes a pool by setting the underlying assets to the top
    * tokens in its category by market cap.
    */
   function reindexPool(uint256 categoryID, uint256 indexSize) external {
@@ -128,19 +265,19 @@ contract PoolController is BNum {
     require(_isBPool[poolAddress], "ERR_NOT_POOL");
     PoolUpdateRecord memory record = _poolUpdateRecords[poolAddress];
     require(
-      (record.index++ % (REWEIGHS_BEFORE_REINDEX + 1)) == REWEIGHS_BEFORE_REINDEX,
-      "ERR_REWEIGH_INDEX"
-    );
-    require(
       now - record.timestamp >= POOL_REWEIGH_DELAY,
       "ERR_POOL_REWEIGH_DELAY"
     );
-    address[] memory tokens = oracle.getTopCategoryTokens(categoryID, indexSize);
-    FixedPoint.uq112x112[] memory prices = oracle.computeAveragePrices(tokens);
+    require(
+      (++record.index % (REWEIGHS_BEFORE_REINDEX + 1)) == 0,
+      "ERR_REWEIGH_INDEX"
+    );
+    address[] memory tokens = _oracle.getTopCategoryTokens(categoryID, indexSize);
+    FixedPoint.uq112x112[] memory prices = _oracle.computeAveragePrices(tokens);
     FixedPoint.uq112x112[] memory weights = Index.computeTokenWeights(tokens, prices);
     uint256[] memory minimumBalances = new uint256[](indexSize);
     uint96[] memory denormalizedWeights = new uint96[](indexSize);
-    uint144 totalValue = estimatePoolValue(BPool(poolAddress));
+    uint144 totalValue = _estimatePoolValue(BPool(poolAddress));
     for (uint256 i = 0; i < indexSize; i++) {
       // The minimum balance is the number of tokens worth
       // the minimum weight of the pool. The minimum weight
@@ -148,7 +285,7 @@ contract PoolController is BNum {
       minimumBalances[i] = prices[i].reciprocal().mul(
         totalValue / 25
       ).decode144();
-      denormalizedWeights[i] = denormalizeFractionalWeight(weights[i]);
+      denormalizedWeights[i] = _denormalizeFractionalWeight(weights[i]);
     }
     BPool(poolAddress).reindexTokens(tokens, denormalizedWeights, minimumBalances);
     record.timestamp = uint128(now);
@@ -163,19 +300,19 @@ contract PoolController is BNum {
     require(_isBPool[poolAddress], "ERR_NOT_POOL");
     PoolUpdateRecord memory record = _poolUpdateRecords[poolAddress];
     require(
-      (record.index++ % (REWEIGHS_BEFORE_REINDEX + 1)) != REWEIGHS_BEFORE_REINDEX,
-      "ERR_REWEIGH_INDEX"
-    );
-    require(
       now - record.timestamp >= POOL_REWEIGH_DELAY,
       "ERR_POOL_REWEIGH_DELAY"
     );
+    require(
+      (++record.index % (REWEIGHS_BEFORE_REINDEX + 1)) != 0,
+      "ERR_REWEIGH_INDEX"
+    );
     address[] memory tokens = BPool(poolAddress).getCurrentDesiredTokens();
-    FixedPoint.uq112x112[] memory prices = oracle.computeAveragePrices(tokens);
+    FixedPoint.uq112x112[] memory prices = _oracle.computeAveragePrices(tokens);
     FixedPoint.uq112x112[] memory weights = Index.computeTokenWeights(tokens, prices);
     uint96[] memory denormalizedWeights = new uint96[](tokens.length);
     for (uint256 i = 0; i < tokens.length; i++) {
-      denormalizedWeights[i] = denormalizeFractionalWeight(weights[i]);
+      denormalizedWeights[i] = _denormalizeFractionalWeight(weights[i]);
     }
     BPool(poolAddress).reweighTokens(tokens, denormalizedWeights);
     record.timestamp = uint128(now);
@@ -183,26 +320,33 @@ contract PoolController is BNum {
   }
 
 /* ---  Queries  --- */
-  function getManager() external view returns (address) {
-    return _manager;
-  }
 
+  /**
+   * @dev Checks if an address is a bpool.
+   */
   function isBPool(address b) external view returns (bool) {
     return _isBPool[b];
   }
 
+  /**
+   * @dev Compute the create2 address for a pool.
+   */
   function computePoolAddress(uint256 categoryID, uint256 indexSize)
     public
     view
-    returns (address)
+    returns (address poolAddress)
   {
-    bytes32 salt = keccak256(abi.encodePacked(categoryID, indexSize));
-    return ProxyLib.computeProxyAddress(
-      _poolContract,
-      salt
+    bytes32 salt = keccak256(abi.encodePacked(
+      POOL_IMPLEMENTATION_ID, categoryID, indexSize
+    ));
+    poolAddress = Create2.computeAddress(
+      salt, PROXY_CODEHASH, address(_proxyManager)
     );
   }
 
+  /**
+   * @dev Gets a pool's update record.
+   */
   function getPoolUpdateRecord(address poolAddress)
     external
     view
@@ -212,7 +356,7 @@ contract PoolController is BNum {
   }
 
   /**
-   * @dev Queries the top n tokens in a category from the market oracle,
+   * @dev Queries the top `indexSize` tokens in a category from the market _oracle,
    * computes their relative weights by market cap square root and determines
    * the weighted balance of each token to meet a specified total value.
    */
@@ -229,40 +373,100 @@ contract PoolController is BNum {
       uint256[] memory balances
     )
   {
-    tokens = oracle.getTopCategoryTokens(categoryID, indexSize);
-    FixedPoint.uq112x112[] memory prices = oracle.computeAveragePrices(tokens);
+    tokens = _oracle.getTopCategoryTokens(categoryID, indexSize);
+    FixedPoint.uq112x112[] memory prices = _oracle.computeAveragePrices(tokens);
     FixedPoint.uq112x112[] memory weights = Index.computeTokenWeights(tokens, prices);
     balances = new uint256[](indexSize);
     denormalizedWeights = new uint96[](indexSize);
     for (uint256 i = 0; i < indexSize; i++) {
       uint144 weightedValue = weights[i].mul(wethValue).decode144();
       balances[i] = uint256(prices[i].reciprocal().mul(weightedValue).decode144());
-      denormalizedWeights[i] = denormalizeFractionalWeight(weights[i]);
+      denormalizedWeights[i] = _denormalizeFractionalWeight(weights[i]);
     }
   }
 
-/* ---  Internal Utility Functions  --- */
-
   /**
-   * @dev Converts a fixed point fraction to a denormalized weight.
-   * Multiply the fraction by the max weight and decode to an unsigned integer.
+   * @dev Queries the top `indexSize` tokens in a category from the market _oracle,
+   * computes their relative weights by market cap square root and determines
+   * the weighted balance of each token to meet a specified total value.
    */
-  function denormalizeFractionalWeight(FixedPoint.uq112x112 memory fraction)
-    internal
-    pure
-    returns (uint96)
+  function getInitialTokensAndBalances(
+    uint256 categoryID,
+    uint256 indexSize,
+    uint256 wethValue
+  )
+    public
+    view
+    returns (
+      address[] memory tokens,
+      uint256[] memory balances
+    )
   {
-    return uint96(fraction.mul(WEIGHT_MULTIPLIER).decode144());
+    tokens = _oracle.getTopCategoryTokens(categoryID, indexSize);
+    FixedPoint.uq112x112[] memory prices = _oracle.computeAveragePrices(tokens);
+    FixedPoint.uq112x112[] memory weights = Index.computeTokenWeights(tokens, prices);
+    balances = new uint256[](indexSize);
+    for (uint256 i = 0; i < indexSize; i++) {
+      uint144 weightedValue = weights[i].mul(wethValue).decode144();
+      balances[i] = uint256(prices[i].reciprocal().mul(weightedValue).decode144());
+    }
   }
+
+/* ---  Internal Pool Functions  --- */
 
   /**
    * @dev Estimate the total value of a pool by taking its first token's
    * "virtual balance" (balance * (totalWeight/weight)) and multiplying
    * by that token's average ether price from UniSwap.
    */
-  function estimatePoolValue(BPool pool) internal view returns (uint144) {
+  function _estimatePoolValue(BPool pool) internal view returns (uint144) {
     (address token, uint256 value) = pool.extrapolatePoolValueFromToken();
-    FixedPoint.uq112x112 memory price = oracle.computeAveragePrice(token);
+    FixedPoint.uq112x112 memory price = _oracle.computeAveragePrice(token);
     return price.mul(value).decode144();
+  }
+
+  /**
+   * @dev Deploy an index pool through the proxy manager.
+   */
+  function _deployPool(uint256 categoryID, uint256 indexSize)
+    internal
+    returns (address pool)
+  {
+    bytes32 salt = keccak256(abi.encodePacked(
+      POOL_IMPLEMENTATION_ID, categoryID, indexSize
+    ));
+    return _proxyManager.deployProxyManyToOne(
+      POOL_IMPLEMENTATION_ID,
+      salt
+    );
+  }
+
+/* ---  Internal Utility Functions  --- */
+
+  /**
+   * @dev Re-assigns a uint128 array to a uint256 array.
+   * This does not affect memory allocation as all Solidity
+   * uint arrays take 32 bytes per item.
+   */
+  function _to256Array(uint128[] memory arr)
+    internal
+    pure
+    returns (uint256[] memory outArr)
+  {
+    assembly {
+      outArr := arr
+    }
+  }
+
+  /**
+   * @dev Converts a fixed point fraction to a denormalized weight.
+   * Multiply the fraction by the max weight and decode to an unsigned integer.
+   */
+  function _denormalizeFractionalWeight(FixedPoint.uq112x112 memory fraction)
+    internal
+    pure
+    returns (uint96)
+  {
+    return uint96(fraction.mul(WEIGHT_MULTIPLIER).decode144());
   }
 }
