c2d20f4870a876fc0bc508e22f37d8e7922843c4
dillon <dillonkellar@protonmail.com>
2020-09-19 16:23:55 -0500

    Added mock unbind token handler

--- contracts/mocks/MockUnbindTokenHandler.sol
+++ contracts/mocks/MockUnbindTokenHandler.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.0;
 
 
-contract MockUnboundTokenSeller {
+contract MockUnbindTokenHandler {
   mapping(address => uint256) internal _receivedTokens;
   function handleUnbindToken(address token, uint256 amount) external {
     _receivedTokens[token] += amount;
