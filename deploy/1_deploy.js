962354c0f73e71d85aeff2a32e36858966f3aa5e
dillon <dillonkellar@protonmail.com>
2020-10-18 20:28:04 -0500

    Cleaned up deployment scripts

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -1,5 +1,5 @@
-const chalk = require('chalk');
-const moment = require('moment');
+const Logger = require('../lib/util/logger');
+const Deployer = require('../lib/util/deployer');
 const { soliditySha3 } = require('web3-utils');
 
 let uniswapFactory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
@@ -10,107 +10,102 @@ const poolInitializerID = soliditySha3('PoolInitializer.sol')
 const poolImplementationID = soliditySha3('IPool.sol');
 const sellerImplementationID = soliditySha3('UnboundTokenSeller.sol');
 
-const Logger = (chainID) => ({
-  info: (v) => {
-    if (chainID != 1 && chainID != 4) return;
-    console.log(
-      chalk.bold.cyan(
-        '@indexed-finance/core/deploy:' + moment(new Date()).format('HH:mm:ss') + ': '
-      ) + v
-    );
-    return v;
-  }
-});
+/*
+Deploys the base contracts for Indexed
+=========================================
+External Dependencies
+- WETH (if not mainnet)
+- Uniswap Factory (if not mainnet or rinkeby)
+- Uniswap Router (if not mainnet or rinkeby)
 
-module.exports = async ({
-  deployments,
-  getChainId,
-  getNamedAccounts,
-  ethers
-}) => {
+Core Contracts
+- Pool Factory
+- Proxy Manager
+- Hourly TWAP Uniswap Oracle
+- Weekly TWAP Uniswap Oracle
+*/
+
+module.exports = async (bre) => {
+  const {
+    deployments,
+    getChainId,
+    getNamedAccounts,
+    ethers
+  } = bre;
   const chainID = await getChainId();
   const logger = Logger(chainID)
-  const { save } = deployments;
   const { deployer } = await getNamedAccounts();
-  const [ signer ] = await ethers.getSigners();
-  // For some reason the contractName field wasn't properly being saved
-  // to deployments.
-  const deploy = async (name, contractName, opts) => {
-    logger.info(`Deploying ${contractName} [${name}]`);
-    const deployment = await deployments.deploy(name, {
-      ...opts,
-      contractName
+  const deploy = await Deployer(bre, logger);
+
+  // ------- External Contracts -------
+  logger.info('Linking external dependencies.', true);
+
+  if (chainID != 1) {
+    const WETH = await deploy('MockERC20', 'weth', {
+      from: deployer,
+      gas: 4000000,
+      args: ["Wrapped Ether V9", "WETH9"]
     });
-    if (deployment.newlyDeployed) {
-      await save(contractName, deployment);
+    weth = WETH.address;
+
+    if (chainID != 4) {
+      logger.info('Deploying UniSwap mocks');
+
+      const factory = await deploy("UniswapV2Factory", 'uniswapFactory', {
+        from: deployer,
+        gas: 4000000,
+        args: [deployer]
+      });
+      uniswapFactory = factory.address;
+  
+      const router = await deploy('UniswapV2Router02', 'uniswapRouter', {
+        from: deployer,
+        gas: 4000000,
+        args: [uniswapFactory, weth]
+      });
+      uniswapRouter = router.address;
     }
-    return deployment;
   }
 
-  logger.info('Executing deployment script.');
 
-
-  const WETH = await deploy('MockERC20', 'weth', {
-    from: deployer,
-    gas: 4000000,
-    args: ["Wrapped Ether V9", "WETH9"]
-  });
-  weth = WETH.address;
-
-  if (chainID != 1 && chainID != 4) {
-    logger.info('Deploying UniSwap mocks');
-
-    const factory = await deploy("UniswapV2Factory", 'uniswapFactory', {
-      from: deployer,
-      gas: 4000000,
-      args: [deployer]
-    });
-    uniswapFactory = factory.address;
-
-    const router = await deploy('UniswapV2Router02', 'uniswapRouter', {
-      from: deployer,
-      gas: 4000000,
-      args: [uniswapFactory, weth]
-    });
-    uniswapRouter = router.address;
-  }
+  // ------- Uniswap Oracles -------
+  let longTermDelay = chainID == 4 ? 60*10 : 3.5*24*60*60;
+  let shortTermDelay = chainID == 4 ? 60*10 - 1 : 60*60;
 
   // Deploy UniSwap oracles
   const longTermUniSwapOracle = await deploy("UniSwapV2PriceOracle", 'WeeklyTWAPUniSwapV2Oracle', {
     from: deployer,
     gas: 4000000,
-    args: [uniswapFactory, weth, 3.5*24*60*60]
-  });
+    args: [uniswapFactory, weth, longTermDelay]
+  }, true);
 
   const shortTermUniswapOracle = await deploy("UniSwapV2PriceOracle", 'HourlyTWAPUniswapV2Oracle', {
     from: deployer,
     gas: 4000000,
-    args: [uniswapFactory, weth, 60*60]
-  });
+    args: [uniswapFactory, weth, shortTermDelay]
+  }, true);
 
+  logger.info('Checking time delay for long oracle');
+  console.log(await longTermUniSwapOracle.OBSERVATION_PERIOD());
+
+  // ------- Core Contracts -------
   // Deploy proxy manager
-  const proxyManagerDeployment = await deploy('DelegateCallProxyManager', 'proxyManager', {
+  const proxyManager = await deploy('DelegateCallProxyManager', 'proxyManager', {
     from: deployer,
     gas: 4000000,
     args: []
-  });
-  const proxyManager = await ethers.getContractAt(
-    proxyManagerDeployment.abi, proxyManagerDeployment.address, signer
-  );
+  }, true);
 
   // Deploy pool factory
-  const poolFactoryDeployment = await deploy('PoolFactory', 'poolFactory', {
+  const poolFactory = await deploy('PoolFactory', 'poolFactory', {
     contractName: 'poolFactory',
     from: deployer,
     gas: 4000000,
     args: [deployer, proxyManager.address]
-  });
-  const poolFactory = await ethers.getContractAt(
-    poolFactoryDeployment.abi, poolFactoryDeployment.address, signer
-  );
+  }, true);
 
   // Deploy pool controller
-  const controllerDeployment = await deploy('MarketCapSqrtController', 'controller', {
+  const controller = await deploy('MarketCapSqrtController', 'controller', {
     from: deployer,
     gas: 4000000,
     args: [
@@ -119,58 +114,64 @@ module.exports = async ({
       poolFactory.address,
       proxyManager.address
     ]
-  });
-  const controller = await ethers.getContractAt(
-    controllerDeployment.abi, controllerDeployment.address, signer
-  );
+  }, true);
 
-  // Add UnboundTokenSeller implementation
-  const tokenSellerImplementation = await deploy('UnboundTokenSeller', 'tokenSellerImplementation', {
-    from: deployer,
-    gas: 4000000,
-    args: [uniswapRouter, shortTermUniswapOracle.address, controller.address]
-  });
-
-  if (tokenSellerImplementation.newlyDeployed) {
-    await proxyManager.createManyToOneProxyRelationship(
-      sellerImplementationID,
-      tokenSellerImplementation.address
-    );
-  }
-
-  // Add IPool implementation
-  const poolImplementation = await deploy('IPool', 'poolImplementation', {
-    from: deployer,
-    gas: 4000000,
-    args: []
-  });
-
-  if (poolImplementation.newlyDeployed) {
-    await proxyManager.createManyToOneProxyRelationship(
-      poolImplementationID,
-      poolImplementation.address
-    );
-  }
-
-  // Add PoolInitializer implementation
-  const poolInitializerImplementation = await deploy('PoolInitializer', 'poolInitializerImplementation', {
-    from: deployer,
-    gas: 4000000,
-    args: [shortTermUniswapOracle.address, controller.address]
-  });
+  // ------- Proxy Implementations -------
+  if (proxyManager.newlyDeployed) {
+    const tokenSellerImplementation = await deploy('UnboundTokenSeller', 'tokenSellerImplementation', {
+      from: deployer,
+      gas: 4000000,
+      args: [uniswapRouter, shortTermUniswapOracle.address, controller.address]
+    });
+    if (tokenSellerImplementation.newlyDeployed) {
+      logger.info('Adding token seller to proxy manager...')
+      await proxyManager.createManyToOneProxyRelationship(
+        sellerImplementationID,
+        tokenSellerImplementation.address,
+        { gasLimit: 400000 }
+      ).then(r => r.wait());
+      logger.success(`Created UnboundTokenSeller.sol implementation on proxy manager.`);
+    }
   
-  if (poolInitializerImplementation.newlyDeployed) {
-    await proxyManager.createManyToOneProxyRelationship(
-      poolInitializerID,
-      poolInitializerImplementation.address
-    );
+    const poolImplementation = await deploy('IPool', 'poolImplementation', {
+      from: deployer,
+      gas: 4000000,
+      args: []
+    });
+  
+    if (poolImplementation.newlyDeployed) {
+      logger.info('Adding pool to proxy manager...')
+      await proxyManager.createManyToOneProxyRelationship(
+        poolImplementationID,
+        poolImplementation.address,
+        { gasLimit: 400000 }
+      ).then(r => r.wait());
+      logger.success(`Created IPool.sol implementation on proxy manager.`);
+    }
+  
+    const poolInitializerImplementation = await deploy('PoolInitializer', 'poolInitializerImplementation', {
+      from: deployer,
+      gas: 4000000,
+      args: [shortTermUniswapOracle.address, controller.address]
+    });
+
+    if (poolInitializerImplementation.newlyDeployed) {
+      logger.info('Adding pool initializer to proxy manager...')
+      await proxyManager.createManyToOneProxyRelationship(
+        poolInitializerID,
+        poolInitializerImplementation.address,
+        { gasLimit: 750000 }
+      ).then(r => r.wait());
+      logger.success(`Created PoolInitializer.sol implementation on proxy manager.`);
+    }
+    await proxyManager.approveDeployer(poolFactory.address, { gasLimit: 60000 }).then(r => r.wait());
+    await proxyManager.approveDeployer(controller.address, { gasLimit: 60000 }).then(r => r.wait());
+    logger.success(`Approved controller and factory to use the proxy manager.`);
   }
-  if (proxyManagerDeployment.newlyDeployed) {
-    await proxyManager.approveDeployer(poolFactory.address);
-    await proxyManager.approveDeployer(controller.address);
-  }
-  if (poolFactoryDeployment.newlyDeployed) {
-    await poolFactory.approvePoolController(controller.address);
+
+  if (poolFactory.newlyDeployed) {
+    await poolFactory.approvePoolController(controller.address, { gasLimit: 60000 }).then(r => r.wait());
+    logger.success(`Approved MarketCapSqrtController.sol to deploy pools through the factory.`);
   }
 };
 
--- deploy/2_deploy.js
+++ deploy/2_deploy.js
@@ -1,84 +1,41 @@
-const chalk = require('chalk');
-const moment = require('moment');
+const Logger = require('../lib/util/logger');
+const Deployer = require('../lib/util/deployer');
 
-const { wrapped_tokens: wrappedTokens } = require('../test/testData/categories.json');
+const testTokens = require('../test/testData/test-tokens.json');
+const { oneToken, toBN, toHex } = require('../lib/util/bn');
 
-const Logger = (chainID) => ({
-  info: (v) => {
-    if (chainID != 1 && chainID != 4) return;
-    console.log(
-      chalk.bold.cyan(
-        '@indexed-finance/core/deploy:' + moment(new Date()).format('HH:mm:ss') + ': '
-      ) + v
-    );
-    return v;
-  }
-});
-
-let uniswapFactory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
-let uniswapRouter = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 
 module.exports = async ({
-  config,
-  deployments,
-  getNamedAccounts,
   getChainId,
-  ethers
+  run
 }) => {
   const chainID = await getChainId();
-  const logger = Logger(chainID);
-  const { save } = deployments;
-  const { deployer } = await getNamedAccounts();
-  const [ signer ] = await ethers.getSigners();
-
-  const deploy = async (name, contractName, opts, returnContract = false) => {
-    logger.info(`Deploying ${contractName} [${name}]`);
-    const deployment = await deployments.deploy(name, {
-      ...opts,
-      contractName
+  const logger = Logger(chainID, 'deploy-mocks');
+  
+  const addresses = [];
+  for (let token of testTokens) {
+    const { marketcap, name, symbol, price } = token;
+    if (!marketcap || !name || !symbol || !price) {
+      throw new Error(`Token JSON must include: marketcap, name, symbol, price`);
+    }
+    const erc20 = await run('deploy-test-token-and-market', { logger, name, symbol });
+    addresses.push(erc20.address);
+    const totalSupply = await erc20.totalSupply();
+    let amountWeth = toBN(marketcap);
+    // let liquidity = marketcap / price;
+    if (totalSupply.eq(0)) {
+      amountWeth = amountWeth.divn(10);
+    }
+    let amountToken = amountWeth.divn(price);
+    await run('add-liquidity', {
+      logger,
+      symbol,
+      amountToken: toHex(amountToken.mul(oneToken)),
+      amountWeth: toHex(amountWeth.mul(oneToken))
     });
-    if (deployment.newlyDeployed) {
-      await save(contractName, deployment);
-    }
-    if (returnContract) {
-      const contract = await ethers.getContractAt(deployment.abi, deployment.address, signer);
-      return contract;
-    }
-    return deployment;
   }
-
-  const weth = await ethers.getContract('weth');
-  if (chainID != 4) {
-    uniswapFactory = await ethers.getContract('uniswapFactory');
-    uniswapRouter = await ethers.getContract('uniswapRouter');
-  } else {
-    uniswapFactory = await ethers.getContractAt('UniswapV2Factory', uniswapFactory, signer);
-    uniswapRouter = await ethers.getContractAt('UniswapV2Router', uniswapRouter, signer);
-  }
-
+  await run('update-prices', { logger, tokens: addresses });
   logger.info('Executing deployment script.');
-  const tokens = [];
-  for (let token of wrappedTokens) {
-    const { name, symbol } = token;
-    const erc20 = await deploy('MockERC20', symbol.toLowerCase(), {
-      from: deployer,
-      gas: 4000000,
-      args: [name, symbol]
-    }, true);
-    const receipt = await uniswapFactory.createPair(
-      erc20.address,
-      weth.address
-    );
-    const { events } = await receipt.wait();
-    const { args: { pair } } = events.filter(e => e.event == 'PairCreated')[0];
-    tokens.push({
-      ...token,
-      token: erc20,
-      address: erc20.address,
-      pair: await ethers.getContractAt('UniswapV2Pair', pair, signer)
-    });
-  }
-  config.wrappedTokens = tokens;
 };
 
 module.exports.tags = ['Mocks'];
