0a975479f1f94a66461ed698d630758b106d0f65
dillon <dillonkellar@protonmail.com>
2020-10-29 21:12:53 -0500

    Improved PoolHelper library

--- test/lib/pool-helper.js
+++ test/lib/pool-helper.js
@@ -1,5 +1,16 @@
-const { calcSpotPrice, calcOutGivenIn, calcSingleInGivenPoolOut, calcInGivenOut, calcInGivenPrice } = require("./calc_comparisons");
+const {
+  calcSpotPrice,
+  calcOutGivenIn,
+  calcSingleInGivenPoolOut,
+  calcInGivenOut,
+  calcInGivenPrice,
+  calcPoolOutGivenSingleIn,
+  calcSingleOutGivenPoolIn,
+  calcPoolInGivenSingleOut
+} = require("./calc_comparisons");
+
 const { default: Decimal } = require("decimal.js");
+const { logger } = require("ethers");
 
 module.exports = class PoolHelper {
   /* token objects should have { address, price, totalSupply, balance } */
@@ -35,6 +46,13 @@ module.exports = class PoolHelper {
     const [token] = this.tokens;
     const { balance, price, denorm } = this.records[token];
     return (this.totalWeight / denorm) * balance * price;
+  }
+
+  get totalWeight() {
+    return this.tokens
+      .map(t => this.records[t])
+      .filter(r => r.denorm)
+      .reduce((total, r) => total + (+r.denorm), 0);
   }
 
   addToken(tokenObj) {
@@ -76,7 +94,6 @@ module.exports = class PoolHelper {
     const marketCapSqrts = marketCaps.map(m => Math.sqrt(+m));
     const sqrtSum = marketCapSqrts.reduce((a, b) => a + b, 0);
     let weights = marketCapSqrts.map(m => 25 * m / sqrtSum);
-    this.totalWeight = weights.reduce((a, b) => a+b, 0);
     for (let i = 0; i < records.length; i++) {
       let a = records[i].address;
       this.records[a].desiredDenorm = weights[i];
@@ -119,6 +136,56 @@ module.exports = class PoolHelper {
     const { denorm: dI, balance: bI } = this.records[tokenIn];
     const { denorm: dO, balance: bO } = this.records[tokenOut];
     return calcSpotPrice(bI, dI, bO, dO, this.swapFee);
+  }
+
+  calcInGivenOut(tokenIn, tokenOut, tokenAmountOut, updateWeightAfter = false, saveUpdate = false) {
+    let { denorm: dI, balance: bI, ready: rI, realBalance: rbI } = this.getInputToken(tokenIn);
+    let { denorm: dO, balance: bO, ready: rO } = this.records[tokenOut];
+    if (!rO) throw new Error('Out token not ready.');
+    let amountIn = calcInGivenOut(bI, dI, bO, dO, tokenAmountOut, this.swapFee);
+    amountIn = (+amountIn);
+    if (amountIn > bI / 2) throw new Error('Exceeds max in ratio');
+    if (tokenAmountOut > bO / 3) throw new Error('Exceeds max out ratio');
+    rbI = rbI + amountIn;
+    
+    if (updateWeightAfter) {
+      const { ready, denorm, balance, realBalance } = this.updateTokenIn({
+        address: tokenIn,
+        balance: bI,
+        ready: rI,
+        denorm: dI,
+        realBalance: rbI
+      });
+      if (ready) {
+        bI = realBalance;
+      } else {
+        bI = balance;
+      }
+      dI = denorm;
+      rI = ready;
+      dO = this.calcWeightDecrease(tokenOut);
+    } else {
+      bI = rbI;
+    }
+    bO = bO - tokenAmountOut;
+    
+    const spotPriceAfter = calcSpotPrice(
+      bI,
+      dI,
+      bO,
+      dO,
+      this.swapFee
+    );
+    if (saveUpdate) {
+      this.records[tokenIn].balance = rbI;
+      this.records[tokenOut].balance = bO;
+      this.records[tokenOut].denorm = dO;
+      if (rI) {
+        this.records[tokenIn].denorm = dI;
+        this.records[tokenIn].ready = true;
+      }
+    }
+    return [Decimal(amountIn), spotPriceAfter];
   }
 
   calcOutGivenIn(tokenIn, tokenOut, amountIn, updateWeightAfter = false, saveUpdate = false) {
@@ -198,7 +265,6 @@ module.exports = class PoolHelper {
         return { ready, denorm, balance, realBalance };
       } else {
         const realToMinRatio = (balance - realBalance) / balance;
-        // (Decimal(balance).minus(Decimal(realBalance))).div(Decimal(balance));
         const weightPremium = (minWeight / 10) * realToMinRatio;
         denorm = minWeight + weightPremium;
         return { ready, denorm, balance, realBalance }
@@ -213,61 +279,139 @@ module.exports = class PoolHelper {
     }
   }
 
-  calcInGivenOut(tokenIn, tokenOut, tokenAmountOut, updateWeightAfter = false) {
-    let { denorm: dI, balance: bI, ready: rI, realBalance: rbI } = this.getInputToken(tokenIn);
-    let { denorm: dO, balance: bO, ready: rO } = this.records[tokenOut];
-    if (!rO) throw new Error('Out token not ready.');
-    let amountIn = calcInGivenOut(bI, dI, bO, dO, tokenAmountOut, this.swapFee);
-    amountIn = (+amountIn);
-    if (amountIn > bI / 2) throw new Error('Exceeds max in ratio');
-    if (tokenAmountOut > bO / 3) throw new Error('Exceeds max out ratio');
-    rbI = rbI + amountIn;
-    
-    if (updateWeightAfter) {
-      const { ready, denorm, balance, realBalance } = this.updateTokenIn({
-        address: tokenIn,
-        balance: bI,
-        ready: rI,
-        denorm: dI,
-        realBalance: rbI
-      });
-      if (ready) {
-        bI = realBalance;
-      } else {
-        bI = balance;
-      }
-      dI = denorm;
-      rI = ready;
-      dO = this.calcWeightDecrease(tokenOut);
-    } else {
-      bI = rbI;
-    }
-    bO = bO - tokenAmountOut;
-    
-    const spotPriceAfter = calcSpotPrice(
-      bI,
-      dI,
-      bO,
-      dO,
-      this.swapFee
-    );
-    return [Decimal(amountIn), spotPriceAfter];
-  }
-
   calcSpotPrice(tokenIn, tokenOut) {
     const { balance: bI, denorm: dI } = this.getInputToken(tokenIn);
     const { denorm: dO, balance: bO } = this.records[tokenOut];
-    return calcSpotPrice(+bI, +dI, +bO, +dO, this.swapFee);
-  }
-  
-  calcPoolOutGivenSingleIn(tokenIn, tokenAmountIn) {
-    const { denorm: dI, balance: bI } = this.records[tokenIn];
-    return calcPoolOutGivenSingleIn(bI, dI, this.poolSupply, this.totalWeight, tokenAmountIn, this.swapFee);
+    return calcSpotPrice(bI, dI, bO, dO, this.swapFee);
   }
 
-  calcSingleInGivenPoolOut(tokenIn, poolAmountOut) {
-    const { denorm: dI, balance: bI } = this.records[tokenIn];
-    return calcSingleInGivenPoolOut(bI, dI, this.poolSupply, this.totalWeight, poolAmountOut, this.swapFee);
+  calcAllInGivenPoolOut(poolAmountOut, applyChanges = false) {
+    let previousPoolBalance = this.poolSupply;
+    let amountsIn = [];
+    for (let tokenIn of this.tokens) {
+      const recordIn = this.getInputToken(tokenIn);
+      const balanceChange = (
+        Decimal(poolAmountOut).div(Decimal(previousPoolBalance))
+      ).mul(recordIn.balance);
+      amountsIn.push(balanceChange.toNumber());
+      recordIn.realBalance = (+recordIn.realBalance) + (balanceChange.toNumber());
+      const { ready, denorm } = this.updateTokenIn({ ...recordIn, address: tokenIn });
+      if (applyChanges) {
+        if (ready) {
+          this.records[tokenIn].denorm = denorm;
+          this.records[tokenIn].ready = true;
+        }
+        this.records[tokenIn].balance = recordIn.realBalance;
+      }
+    }
+    if (applyChanges) {
+      this.poolSupply = (+this.poolSupply) + (+poolAmountOut);
+    }
+    return amountsIn;
+  }
+  
+  calcAllOutGivenPoolIn(poolAmountIn, applyChanges = false) {
+    let previousPoolBalance = this.poolSupply;
+    const amountsOut = [];
+    for (let tokenOut of this.tokens) {
+      const recordOut = this.records[tokenOut];
+      if (!recordOut.ready) {
+        amountsOut.push(0);
+        continue;
+      }
+      const balanceChange = (Decimal(poolAmountIn).div(Decimal(previousPoolBalance))).mul(recordOut.balance);
+      amountsOut.push(balanceChange.toNumber());
+      if (applyChanges) {
+        recordOut.realBalance = (+recordOut.realBalance) - (balanceChange.toNumber());
+        this.records[tokenOut].balance = recordOut.realBalance;
+      }
+    }
+    if (applyChanges) {
+      this.poolSupply = (+this.poolSupply) - (+poolAmountIn);
+    }
+    return amountsOut;
+  }
+
+  calcPoolOutGivenSingleIn(tokenIn, tokenAmountIn, applyChanges = false) {
+    const recordIn = this.getInputToken(tokenIn);
+    const poolAmountOut = calcPoolOutGivenSingleIn(
+      recordIn.balance,
+      recordIn.denorm,
+      this.poolSupply,
+      this.totalWeight,
+      tokenAmountIn,
+      this.swapFee
+    );
+    if (applyChanges) {
+      recordIn.realBalance = (+recordIn.realBalance) + (+tokenAmountIn);
+      const { balance, ready, realBalance, denorm } = this.updateTokenIn({ ...recordIn, address: tokenIn });
+      if (ready) {
+        this.records[tokenIn].denorm = denorm;
+        this.records[tokenIn].ready = true;
+      }
+      this.records[tokenIn].balance = recordIn.realBalance;
+      this.poolSupply = (+this.poolSupply) + (+poolAmountOut);
+    }
+    return poolAmountOut;
+  }
+
+  calcSingleInGivenPoolOut(tokenIn, poolAmountOut, applyChanges = false) {
+    const recordIn = this.getInputToken(tokenIn);
+    const tokenAmountIn = calcSingleInGivenPoolOut(
+      recordIn.balance,
+      recordIn.denorm,
+      this.poolSupply,
+      this.totalWeight,
+      poolAmountOut,
+      this.swapFee
+    );
+    if (applyChanges) {
+      recordIn.realBalance = (+recordIn.realBalance) + (+tokenAmountIn);
+      const { ready, denorm } = this.updateTokenIn({ ...recordIn, address: tokenIn });
+      if (ready) {
+        this.records[tokenIn].denorm = denorm;
+        this.records[tokenIn].ready = true;
+      }
+      this.records[tokenIn].balance = recordIn.realBalance;
+      this.poolSupply = (+this.poolSupply) + (+poolAmountOut);
+    }
+    return tokenAmountIn;
+  }
+
+  calcSingleOutGivenPoolIn(tokenOut, poolAmountIn, applyChanges = false) {
+    const recordOut = this.records[tokenOut];
+    const tokenAmountOut = calcSingleOutGivenPoolIn(
+      recordOut.balance,
+      recordOut.denorm,
+      this.poolSupply,
+      this.totalWeight,
+      poolAmountIn,
+      this.swapFee
+    );
+    if (applyChanges) {
+      this.records[tokenOut].balance = Decimal(recordOut.balance).sub(tokenAmountOut);
+      this.records[tokenOut].denorm = this.calcWeightDecrease(tokenOut);
+      this.poolSupply = (+this.poolSupply) - (+poolAmountIn);
+    }
+    return tokenAmountOut;
+  }
+
+  calcPoolInGivenSingleOut(tokenOut, tokenAmountOut, applyChanges = false) {
+    const recordOut = this.records[tokenOut];
+    const poolAmountIn = calcPoolInGivenSingleOut(
+      recordOut.balance,
+      recordOut.denorm,
+      this.poolSupply,
+      this.totalWeight,
+      tokenAmountOut,
+      this.swapFee
+    );
+    if (applyChanges) {
+      this.records[tokenOut].balance = Decimal(recordOut.balance).sub(tokenAmountOut);
+      this.records[tokenOut].denorm = this.calcWeightDecrease(tokenOut);
+      this.poolSupply = (+this.poolSupply) - (+poolAmountIn);
+    }
+    return poolAmountIn;
   }
 
   calcInGivenPrice(tokenIn, tokenOut, extPrice) {
