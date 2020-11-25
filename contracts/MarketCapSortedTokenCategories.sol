48d452a54d0824b3bd6f17d0846e27d1a81575d4
dillon <dillonkellar@protonmail.com>
2020-11-24 23:39:50 -0600

    Added ability to remove tokens

--- contracts/MarketCapSortedTokenCategories.sol
+++ contracts/MarketCapSortedTokenCategories.sol
@@ -154,6 +154,25 @@ contract MarketCapSortedTokenCategories is Ownable {
     _lastCategoryUpdate[categoryID] -= MAX_SORT_DELAY;
   }
 
+  function removeToken(uint256 categoryID, address token) external onlyOwner validCategory(categoryID) {
+    uint256 i = 0;
+    uint256 len = _categoryTokens[categoryID].length;
+    require(len > 0, "ERR_EMPTY_CATEGORY");
+    for (; i < len; i++) {
+      if (_categoryTokens[categoryID][i] == token) {
+        uint256 last = len - 1;
+        if (i != last) {
+          address lastToken = _categoryTokens[categoryID][last];
+          _categoryTokens[categoryID][i] = lastToken;
+        }
+        _lastCategoryUpdate[categoryID] -= MAX_SORT_DELAY;
+        _categoryTokens[categoryID].pop();
+        return;
+      }
+    }
+    revert("ERR_TOKEN_NOT_FOUND");
+  }
+
   /**
    * @dev Sorts a category's tokens in descending order by market cap.
    * Note: Uses in-memory insertion sort.
@@ -200,7 +219,7 @@ contract MarketCapSortedTokenCategories is Ownable {
       totalSupply,
       LONG_TWAP_MIN_TIME_ELAPSED,
       LONG_TWAP_MAX_TIME_ELAPSED
-    );
+    ); 
   }
 
   /**
